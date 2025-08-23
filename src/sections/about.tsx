6330a6f26472dac9f567d48510d39f768cf47617
TheShiveshNetwork <shiveshtiwari0@gmail.com>
2025-08-23 19:03:24 +0530

    minor modifications and final fixes

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -4,12 +4,12 @@ import { ProfileSection } from "./profile";
 export function AboutSection() {
     return (
         <div className="flex flex-col items-center">
-            <div className="flex flex-col max-w-4xl w-full px-6 md:px-0 mt-10">
-                <div className="text-4xl font-bold">
+            <div className="flex flex-col max-w-4xl w-full mt-10">
+                <div className="text-4xl font-bold px-6 md:px-0">
                     About Me
                 </div>
                 <ProfileSection className="mt-4" />
-                <p className="whitespace-pre-line text-secondary-foreground/60">
+                <p className="whitespace-pre-line text-secondary-foreground/60 px-6 md:px-0">
                     {ABOUT_ME}
                 </p>
             </div>
